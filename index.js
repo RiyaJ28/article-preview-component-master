0eea6e817942bd8b0a459482d5ce87a3755d8892
Riya <21cs74@ecajmer.ac.in>
2023-03-24 20:16:10 +0530

    javascript

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-alert("hello");
+function clickShare() {
+    let popup = document.getElementById("popup");
+    let authorContainer = document.getElementById("AC");
+    let buttonA = document.getElementById("ba");
+    let buttonB = document.getElementById("bb");
+    let mediaMatched = window.matchMedia("(max-width: 600px)").matches;
+
+    console.log(mediaMatched);
+    if (popup.classList.contains("hidden")) {
+      popup.classList.remove("hidden");
+      if (mediaMatched) {
+        buttonA.classList.add("hidden");
+        buttonB.classList.remove("hidden");
+      }
+    } else {
+      popup.classList.add("hidden");
+      if (mediaMatched) {
+        buttonA.classList.remove("hidden");
+        buttonB.classList.add("hidden");
+      }
+    }
+}
